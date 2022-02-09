8c7ea19e56b13909e4049817ef8f4553dcbaba88
Gustavo Mex <gustavoppymex@gmail.com>
2022-02-08 20:08:44 -0600

    ENVIROMENT VARIABLES CONFIGURATION

--- tutorial_apis/settings.py
+++ tutorial_apis/settings.py
@@ -9,18 +9,16 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&zz29cqr$6e0@7j*fv&x6wk0b6m@zm4*=*wmxox+@ewoi2f0*'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
